26a016ec854c5213da8801f0270b055860830aa8
kdonovan2005 <kdonovan2005@gmail.com>
2016-09-16 10:12:35 -0400

    changed default email subject

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -5,7 +5,7 @@ class MessageMailer < ApplicationMailer
   def new_message(message)
     @message = message
 
-    mail subject: "Message from #{message.name}"
+    mail subject: "Message from #{message.name} (#{message.email})"
   end
 
 end
